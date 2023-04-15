51d1a0608562bbb970a926a5066e0ef14aab99ad
Han-nahh <hannatesfaye11@gmail.com>
2023-04-15 15:37:21 +0300

    demo

--- api/index.py
+++ api/index.py
@@ -59,7 +59,7 @@ class TelegramWebhook(BaseModel):
     poll_answer: Optional[dict]
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I have already started!")
 
 def register_handlers(dispatcher):
     start_handler = CommandHandler('start', start)
